274a82072a188f793ea8f9751dbf51b5a4a3ba57
chris-reeves1 <91483522+chris-reeves1@users.noreply.github.com>
2026-04-28 11:47:01 +0100

    333

--- test_app.py
+++ test_app.py
@@ -1,15 +1,40 @@
-import requests
 import unittest
+from app import app
+
 
 class FlaskAppTests(unittest.TestCase):
-    BASE_URL = "http://localhost:80"
+
+    def setUp(self):
+        self.client = app.test_client()
 
     def test_home_page(self):
-        response = requests.get(f"{self.BASE_URL}/")
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Hello", response.text)
-        self.assertIn("I'm currently running in", response.text)
+
+    def test_health_page(self):
+        response = self.client.get("/health")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"ok", response.data.lower())
 
 
 if __name__ == "__main__":
     unittest.main()
+
+
+
+
+# import requests
+# import unittest
+
+# class FlaskAppTests(unittest.TestCase):
+#     BASE_URL = "http://localhost:80"
+
+#     def test_home_page(self):
+#         response = requests.get(f"{self.BASE_URL}/")
+#         self.assertEqual(response.status_code, 200)
+#         self.assertIn("Hello", response.text)
+#         self.assertIn("I'm currently running in", response.text)
+
+
+# if __name__ == "__main__":
+#     unittest.main()
